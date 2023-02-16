88e67d879145cfb72b7732a9b4e45f341403e604
Mai M.belal <mai.belal2127@gmail.com>
2023-02-16 20:48:40 +0200

    update-Password-controller

--- app/Http/Controllers/AbstractAuth/Auth/PasswordController.php
+++ app/Http/Controllers/AbstractAuth/Auth/PasswordController.php
@@ -18,7 +18,7 @@ GuardInterface
     public function update(Request $request): RedirectResponse
     {
         $validated = $request->validateWithBag('updatePassword', [
-            'current_password' => ['required', 'current_password'],
+            'current_password' => ['required', 'current_password'.$this->getGuard],
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
